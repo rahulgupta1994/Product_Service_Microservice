829af7baeb31e7d1fc0c8309229fdd239d2e3fa5
rahul <rahulgkumar7794@gmail.com>
2023-08-15 00:49:18 +0530

    added DTO class in model package

---
+++ src/main/java/com/programmingtechie/productservice/model/ProductDTO.java
@@ -0,0 +1,11 @@
+package com.programmingtechie.productservice.model;
+
+import java.math.BigDecimal;
+
+public class ProductDTO {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+}
